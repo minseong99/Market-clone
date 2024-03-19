4f9d363d14c3aff1b820904978c6b90247b854c7
minseong99 <rlacjfks123@gmail.com>
2024-03-19 21:10:08 +0900

    fix:data space이미지 버그
    data space에서 이미지가 안나오는 버그를 개선하였습니다.

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_image(item_id):
                               WHERE id={item_id}
                               """).fetchone()[0]
     
-    return Response(content=bytes.fromhex(image_bytes))
+    return Response(content=bytes.fromhex(image_bytes), media_type="image/*")
     
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
