aad0ed3cff1bfb4c6e030ba7814fd024b3b7253a
minseong99 <rlacjfks123@gmail.com>
2024-03-21 22:16:25 +0900

    fix:시간 메타 표시 개선
    상품이 등록된지 몇일, 몇달이 지나도 시간만 표시되는 문제를 해결하였습니다.

--- frontend/index.js
+++ frontend/index.js
@@ -5,8 +5,12 @@ const calcTime = (timeStamp) => {
   const hours = passedTime.getHours();
   const minutes = passedTime.getMinutes();
   const seconds = passedTime.getSeconds();
+  const months = passedTime.getMonth();
+  const days = passedTime.getDate();
 
-  if (hours > 0) return ` ${hours}시간 전`;
+  if (months > 0) return ` ${months}달 전`;
+  else if (days > 0) return ` ${days}일 전`;
+  else if (hours > 0) return ` ${hours}시간 전`;
   else if (minutes > 0) return ` ${minutes}분 전`;
   else if (seconds > 0) return ` ${seconds}초 전`;
   else return "방금 전";
