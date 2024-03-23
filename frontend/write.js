580f4e3a075a3031647d8450b1625ffd1f7208f3
minseong99 <rlacjfks123@gmail.com>
2024-03-23 16:49:11 +0900

    fix:글쓰기기능 개선
    유저인 경우만 글쓰기기능을 사용할 수 있도록 수정하였습니다.

--- frontend/write.js
+++ frontend/write.js
@@ -1,37 +1,28 @@
-const checkUser = async () => {
-  const accessToken = window.localStorage.getItem("token");
-  const res = await fetch("/items", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  if (res.status === 401) {
-    alert("로그인이 필요합니다.");
-
-    return true;
-  } else return false;
-};
-
 const handleSubmitForm = async (event) => {
   // 제출은 순간적으로 되기 때문에 preventDefault메소드를 사용해야 한다.
   event.preventDefault();
 
-  if (checkUser()) {
-    window.location.pathname = "/login.html";
-    return;
-  }
-
   const body = new FormData(form);
   // form데이터 보낼때 세계시간 기준
   body.append("insertAt", new Date().getTime());
+
+  const accessToken = window.localStorage.getItem("token");
   try {
     const res = await fetch("/items", {
       method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
       body, // body:body
     });
+    console.log(res.status);
     const data = await res.json();
     //글쓰기 성공(200)이면 페이지를 이동
     if (data == "200") window.location.pathname = "/"; // 루트 페이지로 이동
+    else if (res.status === 401) {
+      alert("로그인을 해주세요.");
+      window.location.pathname = "/login.html";
+    }
   } catch (e) {
     console.log.error(e);
   }
