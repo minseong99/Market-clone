da6e2e2b90e7c0ac0f08623830f1218439355727
minseong99 <rlacjfks123@gmail.com>
2024-03-25 19:47:50 +0900

    svelte, vite로 바꾼 후 firebase deploy 세팅

---
+++ src/pages/Main.svelte
@@ -0,0 +1,71 @@
+<script>
+  let hour = new Date().getHours();
+  let min = new Date().getMinutes();
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}</div>
+    <div class="info-bar__icons">
+      <img src="assets/char-bar.svg" alt="chart-bar" />
+      <img src="assets/wifi.svg" alt="wifi-bar" />
+      <img src="assets/battery.svg" alt="battery-bar" />
+    </div>
+  </div>
+
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div>역삼1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/arrow-down.svg" alt="arrow-down" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" alt="search" />
+      <img src="assets/menu.svg" alt="menu" />
+      <img src="assets/alert.svg" alt="alert" />
+    </div>
+  </div>
+</header>
+
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/home.svg" />
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/doc.svg" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/location.svg" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/chat.svg" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/user.svg" />
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">
+  <div class="media-info-msg-text">화면 사이즈를 줄여주세요.</div>
+</div>
