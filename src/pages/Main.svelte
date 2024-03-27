f628666a6f1d1f96974c2fec78ad865831cb6b30
minseong99 <rlacjfks123@gmail.com>
2024-03-27 10:01:25 +0900

    feat:firebase로 글쓰기 로직 완성
    firebase를 활용하여 글쓰기 데이터를 db에 저장하고 꺼내서 홈에 보이도록 하는 것까지 만들었습니다.

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,6 +1,43 @@
 <script>
+  import { onMount } from "svelte";
+  import Footer from "../components/Footer.svelte";
+  import { getDatabase, ref, onValue } from "firebase/database";
+
   let hour = new Date().getHours();
   let min = new Date().getMinutes();
+
+  // $: items : items가 반응형 변수로 선언이 된다. 즉, items의 값이 바뀌게 되면  html부분에서 items를 랜더링하고있는 태그에서
+  //             자동으로 값이 바뀌면 화면이 바뀐다.
+  $: items = [];
+
+  const calcTime = (timeStamp) => {
+    // 한국시간 UTC + 9
+    const curTime = new Date().getTime() - 9 * 60 * 60 * 1000;
+    const passedTime = new Date(curTime - timeStamp);
+    const hours = passedTime.getHours();
+    const minutes = passedTime.getMinutes();
+    const seconds = passedTime.getSeconds();
+    const months = passedTime.getMonth();
+    const days = passedTime.getDate();
+
+    if (months > 0) return ` ${months}달 전`;
+    else if (days > 1) return ` ${days}일 전`;
+    else if (hours > 0) return ` ${hours}시간 전`;
+    else if (minutes > 0) return ` ${minutes}분 전`;
+    else if (seconds > 0) return ` ${seconds}초 전`;
+    else return "방금 전";
+  };
+
+  const db = getDatabase();
+  const itemsRef = ref(db, "items/");
+  // onValue : itemsRef를 받는데 itemsRef가 바뀔때마다 snapshot이 새롭게 내려온다.
+  onMount(() => {
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      //Object.values() : value들만 가져와서 array를 만들어준다
+      items = Object.values(data).reverse();
+    });
+  });
 </script>
 
 <header>
@@ -29,43 +66,28 @@
 </header>
 
 <main>
+  {#each items as item}
+    <div class="items-list">
+      <div class="items-list__img">
+        <img alt={item.title} src={item.imgUrl} />
+      </div>
+      <div class="items-list__info">
+        <div class="items-list__info-title">{item.title}</div>
+        <div class="items-list__info-meta">
+          {item.place}
+          {calcTime(item.insertAt)}
+        </div>
+        <div class="items-list__info-price">{item.price}</div>
+
+        <div>{item.description}</div>
+      </div>
+    </div>
+  {/each}
   <a class="write-btn" href="#/write">+ 글쓰기</a>
 </main>
 
-<footer>
-  <div class="footer-block">
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/home.svg" />
-      </div>
-      <div class="footer-icons__desc">홈</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/doc.svg" />
-      </div>
-      <div class="footer-icons__desc">동네생활</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/location.svg" />
-      </div>
-      <div class="footer-icons__desc">내 근처</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/chat.svg" />
-      </div>
-      <div class="footer-icons__desc">채팅</div>
-    </div>
-    <div class="footer-icons">
-      <div class="footer-icons__img">
-        <img src="assets/user.svg" />
-      </div>
-      <div class="footer-icons__desc">나의 당근</div>
-    </div>
-  </div>
-</footer>
+<Footer location="home" />
+
 <div class="media-info-msg">
   <div class="media-info-msg-text">화면 사이즈를 줄여주세요.</div>
 </div>
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,25 +1,96 @@
-<form id="write-form">
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import Footer from "../components/Footer.svelte";
+  // 참조가 겹쳐서 ref를 refImage로 바꾸었다.
+  import {
+    getDownloadURL,
+    getStorage,
+    ref as refImage,
+    uploadBytes,
+  } from "firebase/storage";
+  let title;
+  let price;
+  let description;
+  let place;
+  let files;
+
+  const storage = getStorage();
+  const db = getDatabase();
+
+  // 반응형으로 files가 바뀔때마다 console.log(files)가 찍히게 된다.
+  // $: if (files) console.log(files);
+
+  const uploadFile = async () => {
+    // 이미지 용량 최적화 과제
+    const file = files[0];
+    const name = file.name;
+    const imgRef = refImage(storage, name);
+    const res = await uploadBytes(imgRef, file);
+    // url을 클릭하면 실제 이미지를 볼 수 있음
+    const url = await getDownloadURL(imgRef);
+
+    return url;
+  };
+  async function writeUserData(imgUrl) {
+    // set으로 하면 기존에 있던 목록의 이름과 겹치면 그것이 업데이트 되기 때문에 push사용
+    await push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+      insertAt: new Date().getTime(),
+      imgUrl,
+    });
+
+    window.location.hash = "/";
+  }
+
+  const handleSubmit = async () => {
+    const imgUrl = await uploadFile();
+    writeUserData(imgUrl);
+  };
+</script>
+
+<form id="write-form" on:submit|preventDefault={handleSubmit}>
   <div>
     <label for="image">이미지</label>
-    <input type="file" id="image" name="image" />
+    <input type="file" bind:files id="image" name="image" />
   </div>
   <div>
     <label for="title">제목</label>
-    <input type="text" id="title" name="title" />
+    <input type="text" id="title" name="title" bind:value={title} />
   </div>
   <div>
     <label for="price">가격</label>
-    <input type="number" id="price" name="price" />
+    <input type="number" id="price" name="price" bind:value={price} />
   </div>
   <div>
     <label for="description">설명</label>
-    <input type="text" id="description" name="description" />
+    <input
+      type="text"
+      id="description"
+      name="description"
+      bind:value={description}
+    />
   </div>
   <div>
     <label for="place">장소</label>
-    <input type="text" id="place" name="place" />
+    <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
-    <button type="submit">글쓰기완료</button>
+    <button class="write-button" type="submit">글쓰기완료</button>
   </div>
 </form>
+
+<Footer location="write" />
+
+<style>
+  .write-button {
+    background-color: tomato;
+    margin: 10px;
+    border-radius: 10px;
+    padding: 5px 12px 5px 12px;
+    color: white;
+    cursor: pointer;
+  }
+</style>
