7bc7e2bcec031804047401ecd48db398bbc30259
minseong99 <rlacjfks123@gmail.com>
2024-03-31 17:19:16 +0900

    .env파일을 만들어 중요한 정보는 배포가 안되게 하였습니다.

--- firebase.js
+++ firebase.js
@@ -6,14 +6,13 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyB9GUI0gJ2FH-E8hpRfXimrpDE4Czcrarw",
-  authDomain: "carrot-market-b7d7b.firebaseapp.com",
-  databaseURL:
-    "https://carrot-market-b7d7b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "carrot-market-b7d7b",
-  storageBucket: "carrot-market-b7d7b.appspot.com",
-  messagingSenderId: "603522085668",
-  appId: "1:603522085668:web:01b4694f84ede07ddd39b1",
+  apiKey: import.meta.env.VITE_APP_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINN_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
